6f9089ba841dc86492203998f1657b48b17036b3
İsmail Efe Ataş <96317271+efe-atas@users.noreply.github.com>
2024-10-15 23:31:01 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "efeatas",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "computer engineering student",
+    bio: "I develop everything i can",
+    email: "iefeatas@gmail.com",
+    linkedin: "efeatas",
+    github: "efe-atas",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `efe-atas`,
+      href: "https://github.com/efe-atas/blog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "efe-atas",
+    description: "welcome to efe-atas!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
@@ -54,7 +54,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
