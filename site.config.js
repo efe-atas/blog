c5c4cbfea8ca1b0e9b366ef245ecd7dc71131c29
İsmail Efe Ataş <96317271+efe-atas@users.noreply.github.com>
2024-10-16 22:12:00 +0300

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -36,9 +36,9 @@ const CONFIG = {
 
   // plugin configuration (optional)
   googleAnalytics: {
-    enable: false,
+    enable: true,
     config: {
-      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "G-WBTKTXFT2L",
     },
   },
   googleSearchConsole: {
